00763f812fec074f9535a740c4d3f3038e38881a
Atul <atul@ATUL-PC>
2020-09-12 23:10:27 +0530

    Updates

---
+++ precache-manifest.db7ef2930eb66cc8c363b284acd55019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e199df20734e60c31cb29dff415823",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "17a264b5080d88ab5f0f",
+    "url": "/ExpenseTracker/static/css/main.8a50243e.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/ExpenseTracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ExpenseTracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a264b5080d88ab5f0f",
+    "url": "/ExpenseTracker/static/js/main.c5858b6d.chunk.js"
+  },
+  {
+    "revision": "bce0d28d436fe3899340",
+    "url": "/ExpenseTracker/static/js/runtime-main.a7edbd44.js"
+  }
+]);
